70cd4e176399b303685f2b30f32cef7d86e1e988
josue <jramiroem@gmail.com>
2022-10-04 14:48:04 -0400

    created find dialog class

---
+++ MRUI/finddialog.h
@@ -0,0 +1,17 @@
+#ifndef FINDDIALOG_H
+#define FINDDIALOG_H
+#include <QDialog>
+
+class QCheckbox;
+class QLabel;
+class QCheckbox;
+class QPushbutton;
+
+class FindDialog : QDialog
+{
+    Q_OBJECT
+public:
+    FindDialog(QWidget *parent = nullptr);
+};
+
+#endif // FINDDIALOG_H
